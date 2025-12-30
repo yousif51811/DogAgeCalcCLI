cf5384697cb9abecac354a1f5c6f865796cf14b6
Ownerbaby <slogxtt@gmail.com>
2025-12-30 04:30:47 +0400

    Created The Dog Years Calculator - Added Logic to convert human years to dog years Using multiple formulas - Added safety to make sure No invalid values are entered

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter Your Age: ");
+string? Input = Console.ReadLine();
+double Age =  Convert.ToDouble(Input);
+if ( Age <= 1 && Age > 0)
+{
+    double DogyearsAge = 15 * Age;
+    Print(DogyearsAge);
+}
+else if (Age <= 2 && Age > 1)
+{
+    double DogyearsAge = ((Age -1) * 5) + 15;
+    Console.WriteLine("Your Age in Dog Years is: " + DogyearsAge);
+    Print(DogyearsAge);
+}
+else if (Age > 2)
+{
+    double DogyearsAge = ((Age - 2) * 5) + 24;
+    Print(DogyearsAge);
+}
+else
+{
+    Console.WriteLine("Enter A Valid Number");
+}
+static void Print(double Dogyears)
+{
+    Console.WriteLine("Your Age in Dog Years is: " + Dogyears);
+}
