0f605d9f969031ec968371309fc0b3b33d5caa47
Ownerbaby <slogxtt@gmail.com>
2025-12-30 04:58:08 +0400

    Improved README and bug fixes - Added a better and more detailed readme - Fixed logic issue - Added more comments to make code easier to read

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
-﻿Console.Write("Enter Your Age: ");
+﻿// Made by Yousif51811
+// Dog years calculation logic:
+// First year: + 15
+// Second year: + 9
+// Anything After: + 5
+
+Console.Write("Enter Your Age: ");
 string? Input = Console.ReadLine();
 double Age =  Convert.ToDouble(Input);
 if ( Age <= 1 && Age > 0)
@@ -8,19 +14,24 @@ if ( Age <= 1 && Age > 0)
 }
 else if (Age <= 2 && Age > 1)
 {
-    double DogyearsAge = ((Age -1) * 5) + 15;
+    double DogyearsAge = ((Age -1) * 9) + 15;
     Console.WriteLine("Your Age in Dog Years is: " + DogyearsAge);
     Print(DogyearsAge);
 }
+
 else if (Age > 2)
 {
     double DogyearsAge = ((Age - 2) * 5) + 24;
     Print(DogyearsAge);
 }
-else
+
+// Account for invalid values (Negative Numbers, Zero, Characters, etc...)
+else 
 {
-    Console.WriteLine("Enter A Valid Number");
+    Console.WriteLine("Enter A Valid Number"); 
 }
+
+// Function to print Age to prevent reusing the same line multiple times
 static void Print(double Dogyears)
 {
     Console.WriteLine("Your Age in Dog Years is: " + Dogyears);
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# DogAgeCalcCLI
-Calculates human to Dog years Age in C#
+# Dog Years Calculator CLI
+A Simple C# Program to convert human years to dog years
+
+## How to run
+1. Make sure you have the .NET sdk installed
+2. Clone the repository 
+```
+git clone https://github.com/yousif51811/DogAgeCalcCLI
+```
+3. Run the Program
+``` 
+Dotnet run
+```
+4. Enjoy!
+
+## How it works
+#### h = Human years
+### 1 => Age > 0
+```h x 15```
+### 1 => Age > 0
+```((h - 1) x 9) + 15```
+### Age > 2
+```((h-2) x 5) + 24```
